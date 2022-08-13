9e1a532b9789fb68dac19de74610e36d3bbcb1a4
Victor  Kanu <victorchimakanu@gmail.com>
2022-08-13 02:30:16 +0100

    3. Testing Contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,5 +4,16 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {
-    string public name = "My Token";
+    string public name = "Dapp University";
+    string public symbol ;
+    uint256 public decimals = 18;
+    uint256 public totalSupply; 
+
+    constructor (string memory _name, string memory _symbol, uint256 _totalSupply) {
+    name = _name;
+    symbol = _symbol;
+    totalSupply = _totalSupply * (10**decimals); 
+    }
+
+
 }
